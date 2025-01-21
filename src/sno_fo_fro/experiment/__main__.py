793c6b989ebb257ec8d1976c70d9f197d0d38ab9
Osogi <dragonmail135159@gmail.com>
2025-01-22 00:21:59 +0300

    refactor: naming

--- src/sno_fo_fro/experiment/__main__.py
+++ src/sno_fo_fro/experiment/__main__.py
@@ -1,60 +1,57 @@
-import random
-from scipy import stats
 from sno_fo_fro.experiment.experimenter import (
     Experimenter,
     ExperimenterCompareMode,
     ExperimenterWeather,
-    SampleAnalyzer,
 )
 from sno_fo_fro.hypotheses import (
     ImageBlurrinessProcessor,
     ImageLuminanceProcessor,
     ImageContrastProcessor,
     ImageSaturationProcessor,
-    ImageSegmentsProcessor,
+    ImageSegmentsSharpnessProcessor,
     ImageWhitenessProcessor,
-    ImageWhiteNoiseProcessor,
+    ImageWhiteGradientProcessor,
     ImageColdnessProcessor,
     ImageEdgeDensityProcessor,
 )
 
 
 img_proc_and_idea = [
-    # (
-    #     ImageBlurrinessProcessor(),
-    #     (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS),
-    # ),
-    # (
-    #     ImageLuminanceProcessor(False),
-    #     (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
-    # ),
-    # (
-    #     ImageLuminanceProcessor(True),
-    #     (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
-    # ),
-    # (ImageContrastProcessor(), (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS)),
-    # (
-    #     ImageSaturationProcessor(),
-    #     (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS),
-    # ),
-    # (
-    #     ImageWhiteNoiseProcessor(),
-    #     (ExperimenterWeather.SNOW, ExperimenterCompareMode.GREATER),
-    # ),
-    # (
-    #     ImageWhitenessProcessor(),
-    #     (ExperimenterWeather.SNOW, ExperimenterCompareMode.GREATER),
-    # ),
-    # (
-    #     ImageColdnessProcessor(),
-    #     (ExperimenterWeather.FROST, ExperimenterCompareMode.LESS),
-    # ),
-    # (
-    #     ImageEdgeDensityProcessor(),
-    #     (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
-    # ),
     (
-        ImageSegmentsProcessor(),
+        ImageBlurrinessProcessor(),
+        (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS),
+    ),
+    (
+        ImageLuminanceProcessor(False),
+        (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
+    ),
+    (
+        ImageLuminanceProcessor(True),
+        (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
+    ),
+    (ImageContrastProcessor(), (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS)),
+    (
+        ImageSaturationProcessor(),
+        (ExperimenterWeather.FOG, ExperimenterCompareMode.LESS),
+    ),
+    (
+        ImageWhiteGradientProcessor(),
+        (ExperimenterWeather.SNOW, ExperimenterCompareMode.GREATER),
+    ),
+    (
+        ImageWhitenessProcessor(),
+        (ExperimenterWeather.SNOW, ExperimenterCompareMode.GREATER),
+    ),
+    (
+        ImageColdnessProcessor(),
+        (ExperimenterWeather.FROST, ExperimenterCompareMode.LESS),
+    ),
+    (
+        ImageEdgeDensityProcessor(),
+        (ExperimenterWeather.FROST, ExperimenterCompareMode.GREATER),
+    ),
+    (
+        ImageSegmentsSharpnessProcessor(),
         (ExperimenterWeather.SNOW, ExperimenterCompareMode.GREATER),
     ),
 ]
